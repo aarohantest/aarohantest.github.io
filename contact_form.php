0b45dbfcc75976149ce2d89780e39f2a06bdc09c
aarohantest <aarohan.gcharnalia@gmail.com>
2016-11-16 02:26:18 +0530

    Added contact form and compressed a few images

---
+++ contact_form.php
@@ -0,0 +1,37 @@
+<?php
+    // Fetching Values from URL.
+    $name = trim($_POST['name1']);
+    $email = trim($_POST['email1']);
+    $message = trim($_POST['message1']);
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing E-mail address.
+    // After sanitization Validation is performed
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $subject = "Website visitor query from: ".$name;
+            // To send HTML mail, the Content-type header must be set.
+            $headers = 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+            $headers .= 'From:' . $email. "\r\n"; // Sender's Email
+            $sendmessage = "<div style=\"background-color:#7E7E7E; color:white;\">" . $message . "</div>";
+            // Message lines should not exceed 70 characters (PHP rule), so wrap it.
+            $sendmessage = wordwrap($sendmessage, 70);
+            // Send mail by PHP Mail Function.
+            mail("aarohan.querry@gmail.com", $subject, $sendmessage, $headers);
+
+            $subject = "Contact Confirmation mail";
+            $headers = 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+            $headers .= 'From: aarohan.querry@gmail.com \r\n';
+            $template = '<div style="padding:50px; color:white;">Hello ' . $name . ',<br/>'
+                . '<br/>Thank you...! For Contacting Us.<br/><br/>'
+                . 'This is a Contact Confirmation mail.'
+                . '<br/>'
+                . 'We Will contact You as soon as possible .</div>';
+            $sendmessage = "<div style=\"background-color:#7E7E7E; color:white;\">" . $template . "</div>";
+            $sendmessage = wordwrap($sendmessage, 70);
+            mail($email, $subject, $sendmessage, $headers);
+            echo "Your Query has been received, We will contact you soon.";
+        }
+    } else {
+        echo "<span>* invalid email *</span>";
+    }
+?>
--- js/script.js
+++ js/script.js
@@ -464,6 +464,29 @@ $(document).ready(function () {
                 $("#acting path").attr("fill","#000");
         });
     });
+    $("#send").click(function() {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var message = $("#message").val();
+        // $("#returnmessage").empty(); // To empty previous error/success message.
+        // Checking for blank fields.
+        if (name == '' || email == '' || message == '') {
+            alert("Please Fill Required Fields");
+        } else {
+            // Returns successful data submission message when the entered information is sent.
+            $.post("contact_form.php", {
+                name1: name,
+                email1: email,
+                message1: message
+            }, function(data) {
+                // $("#returnmessage").append(data); // Append returned message to message paragraph.
+                if (data == "Your Query has been received, We will contact you soon.") {
+                    alert("Your Query has been received, We will contact you soon.");
+                    $("#form")[0].reset(); // To reset form fields on success.
+                }
+            });
+        }
+    });
 });
 $(window).load(function () {
     $(".preloader").delay(2000).fadeOut("slow");
